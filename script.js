519bb4faf4c06a1ccab369d0e4e590b18d1bdb0b
Jon Monarrez <135302142+j0n-m@users.noreply.github.com>
2023-08-01 13:43:43 -0500

    Add winner logic to game functionality

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ const gameBoard = () => {
   let rows = 3;
   let columns = 3;
   const board = [];
+  const gameEnd = null;
 
   for (let i = 0; i < rows; i++) {
     board.push([]);
@@ -59,14 +60,16 @@ function gameController(allowCPU = false, playerMarks = ['X', 'O']) {
     const getCount = () => count;
     const getRound = () => round;
     function displayCount() {
+      return count;
+    }
+    function displayRound() {
       if (count > 0 && count % 2 == 0) {
         console.log(`Round: ${++round}`);
       } else {
         console.log(`Round: ${round}`);
       }
-
     }
-    return { increment, getCount, displayCount, getRound };
+    return { increment, getCount, displayRound, displayCount, getRound };
   })();
   function switchTurn() {
     if (activePlayerTurn == currentPlayers[0]) {
@@ -74,24 +77,58 @@ function gameController(allowCPU = false, playerMarks = ['X', 'O']) {
     } else {
       activePlayerTurn = currentPlayers[0];
     }
-    console.log(`new active player is: ${activePlayerTurn.getName()}`);
+  }
+  function checkForWinner(playerObj) {
+    let rowLength = board.getBoard()[0].length;
+    const straightRow = [playerObj.getMark(), playerObj.getMark(), playerObj.getMark()];
+
+    const straightColumn = [playerObj.getMark(), playerObj.getMark(), playerObj.getMark()];
+    //check for all rows
+    for (let i = 0; i < rowLength; i++) {
+      if (board.getBoard()[i].toString() == straightRow) {
+        return true;
+      }
+    }
+    //check for all columns
+    if (board.getBoard()[0][0] == playerObj.getMark() && board.getBoard()[1][0] == playerObj.getMark() && board.getBoard()[2][0] == playerObj.getMark() || board.getBoard()[0][1] == playerObj.getMark() && board.getBoard()[1][1] == playerObj.getMark() && board.getBoard()[2][1] == playerObj.getMark() || board.getBoard()[0][2] == playerObj.getMark() && board.getBoard()[1][2] == playerObj.getMark() && board.getBoard()[2][2] == playerObj.getMark()) {
+      return true;
+    }
+
+    //check for all diagnols
+    if (board.getBoard()[0][0] == playerObj.getMark() && board.getBoard()[1][1] == playerObj.getMark() && board.getBoard()[2][2] == playerObj.getMark() || board.getBoard()[0][2] == playerObj.getMark() && board.getBoard()[1][1] == playerObj.getMark() && board.getBoard()[2][0] == playerObj.getMark()) {
+      return true;
+    }
+  }
+  function gameOver(player, winnerFound) {
+    return (winnerFound) ? console.log(`${player.getName()} wins!`) : console.log('Game is over, it\'s a tie');
   }
   function playRound(indexRow, indexColumn) {
     if (board.getBoard()[indexRow][indexColumn]) {
       console.log('Cannot place your mark here');
       return;
     }
-    gameCounter.displayCount();
+    gameCounter.displayRound();
 
 
     board.addPlayerMark(activePlayerTurn.getMark(), indexRow, indexColumn);
+    gameCounter.increment();
+    console.log(`game count -> ${gameCounter.getCount()}`);
+
     board.printBoard();
-    if (gameCounter.getRound() == 3) {
-      console.log('Checking for winners');
+    if (gameCounter.getRound() >= 3) {
+      let foundWinner;
+      foundWinner = checkForWinner(activePlayerTurn);
+      if (foundWinner) {
+        gameOver(activePlayerTurn, foundWinner);
+        return;
+      }
+      if (gameCounter.getCount() == 9) {
+        gameOver(activePlayerTurn, foundWinner);
+        return;
+      }
     }
     switchTurn();
     displayPlayerTurn();
-    gameCounter.increment();
 
   }
   function displayPlayerTurn() {
